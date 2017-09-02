8094295be43b3673e412789d6299e5dbf685026b
Angel Hojsgaard <neto.angel@gmail.com>
2017-09-01 22:33:29 -0300

    Me - added Calculator class with methods and Calculator test. Applied jacoco report coverage

--- jacocoCalcuTest/src/test/java/test/project/CalculatorTest.java
+++ jacocoCalcuTest/src/test/java/test/project/CalculatorTest.java
@@ -1,38 +1,46 @@
 package test.project;
 
-import junit.framework.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for Jacoco calcu test.
  */
 public class CalculatorTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public CalculatorTest( String testName )
-    {
-        super( testName );
-    }
+	private Calculator jacocoCalcuTest;
+	
+	@Before
+	  public void setUp() throws Exception {
+	    this.jacocoCalcuTest = new Calculator();
+	  }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( CalculatorTest.class );
-    }
+	  @After
+	  public void tearDown() throws Exception {
+		  jacocoCalcuTest = null;
+	  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	  @Test
+	  public void testSumaNumeroPositivos() {
+	    assertEquals("Suma", 7.0, jacocoCalcuTest.sumar(3, 4));
+	  }
+	  
+	  @Test
+	  public void testRestaNumeroPositivos() {
+	    assertEquals("Resta", 3.0, jacocoCalcuTest.restar(4, 1));
+	  }
+	  
+	  @Test
+	  public void testDivideNumeroPositivos() {
+	    assertEquals("Division", 3.0, jacocoCalcuTest.dividir(15, 5));
+	  }
+	  
+	  @Test
+	  public void testMultiplicaNumeroPositivos() {
+	    assertEquals("Suma", 12.0, jacocoCalcuTest.multiplicar(3, 4));
+	  }
 }
